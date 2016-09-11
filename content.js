2c252dcc4af18f689216d7a89fed42bf382b52cb
Avi Vaid <avaid1996@gmail.com>
2016-09-11 00:15:25 -0700

    added alerts with the headlines- fetching them using a jquery get on the links
    Signed-off-by: Avi Vaid <avaid1996@gmail.com>

--- content.js
+++ content.js
@@ -1,9 +1,22 @@
 // content.js
 var pageSRC = $( 'body' ).html().toString();
 var topicREG = /\/topic\/[^"]*/g;
+var facebookPREF = "https://www.facebook.com"
 var urlList = pageSRC.match(topicREG);
-console.log(urlList)
 for (var i = 0; i < urlList.length; i++) {
-		console.log(i)
-		console.log(urlList[i])
+		console.log(i);
+		var builtURL = facebookPREF+urlList[i];
+		var newsReq = $.get(builtURL, function(pageSRC, success) {
+				if(success=="success") {
+					// looked ahead and got headline- parse headline
+					getRawHeadLineReg = /<div\sclass="_4l5g">.*<div class="_4l5h">[^<]*/g
+					rawHeadLine = pageSRC.match(getRawHeadLineReg).toString();
+					headLine = rawHeadLine.replace(/<div[^>]*>/g, '');
+					headLine = headLine.replace(/<\/div>/g, '\n -------------------------------------------------------- \n')
+					headLine = headLine.replace(/&#039;/g, "'");
+
+					alert(headLine)
+					// have headline- carry on with code replacement 
+				}
+		});
 }
